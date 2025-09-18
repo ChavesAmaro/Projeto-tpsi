6b46badb49a757601552673d07ad8a265271bb40
ChavesAmaro <gon94chavesamaro@gmail.com>
2025-09-18 13:12:32 +0100

    Várias alterações

--- Lista de Livros django/livros/urls.py
+++ Lista de Livros django/livros/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('books/<int:book_id>/toggle/', views.toggle_book, name='toggle_book'),
     path('books/<int:book_id>/delete/', views.delete_book, name='delete_book'),
     path('books/<int:book_id>/edit/', views.edit_book, name='edit_book'),
+    path("stats/", views.stats_view, name="stats"),
 ]
--- Lista de Livros django/livros/views.py
+++ Lista de Livros django/livros/views.py
@@ -1,6 +1,9 @@
 import re
 from django.db.models import Q
+from django.db.models import Count
+from django.db.models.functions import ExtractYear
 from django.shortcuts import render, redirect, get_object_or_404
+from django.db.models import Count, Avg, Sum, F
 from .models import Book
 from django.utils import timezone
 from datetime import datetime
@@ -97,6 +100,13 @@ def edit_book(request, book_id):
             book.date_start = datetime.strptime(date_start_str, "%Y-%m-%d").date()
         else:
             book.date_start = None
+        date_read_str = request.POST.get("date_read")
+        if date_read_str:
+            book.date_read = datetime.strptime(date_read_str, "%Y-%m-%d").date()
+            book.read = True  # marca como lido se tiver data
+        else:
+            book.date_read = None
+            book.read = False
         book.ano_publicacao = int(request.POST.get("ano_publicacao")) if request.POST.get("ano_publicacao") else None
         book.paginas = int(request.POST.get("paginas")) if request.POST.get("paginas") else None
         book.tema_principal = request.POST.get("tema_principal")
@@ -106,3 +116,69 @@ def edit_book(request, book_id):
         book.save()
         return redirect("book_list")
     return render(request, "livros/edit_book.html", {"book": book})
+
+def stats_view(request):
+    total_livros = Book.objects.count()
+    livros_lidos = Book.objects.filter(read=True, paginas__isnull=False)
+    lidos = Book.objects.filter(read=True).count()
+    nao_lidos = Book.objects.filter(read=False).count()
+    media_paginas = Book.objects.aggregate(Avg("paginas"))["paginas__avg"] or 0
+    media_avaliacao = Book.objects.aggregate(Avg("avaliacao_pessoal"))["avaliacao_pessoal__avg"] or 0
+    preco_total = Book.objects.aggregate(Sum("preco"))["preco__sum"] or 0
+    preco_total = round(preco_total, 2)
+
+    paginas_por_ano = {}
+    for livro in livros_lidos:
+        if livro.date_read:
+            ano = livro.date_read.year
+            paginas_por_ano.setdefault(ano, []).append(livro.paginas)
+            
+    media_total_por_ano = {}
+    total_paginas = 0
+
+    for ano, paginas in paginas_por_ano.items():
+        media_total_por_ano[ano] = {
+        "media": sum(paginas) / len(paginas),
+        "total": sum(paginas)
+        }
+        total_paginas += sum(paginas)
+    
+    paginas_por_ano_ordenado = dict(sorted(media_total_por_ano.items()))
+    
+    avaliacoes = (
+        Book.objects
+        .values("avaliacao_pessoal")
+        .annotate(total=Count("id"))
+        .order_by("avaliacao_pessoal")
+    )
+    
+    books_by_year = (
+        Book.objects.filter(read=True)
+        .annotate(year=ExtractYear("date_read"))
+        .values("year")
+        .annotate(total=Count("id"))
+        .order_by("year")
+    )
+
+    years = [b["year"] for b in books_by_year]
+    totals = [b["total"] for b in books_by_year]
+
+    
+    
+    labels = [a["avaliacao_pessoal"] for a in avaliacoes if a["avaliacao_pessoal"]]
+    valores = [a["total"] for a in avaliacoes if a["avaliacao_pessoal"]]
+    
+    return render(request, "livros/stats.html", {
+        "total_livros": total_livros,
+        "lidos": lidos,
+        "nao_lidos": nao_lidos,
+        "media_paginas": round(media_paginas, 1),
+        "media_avaliacao": round(media_avaliacao, 1),
+        "preco_total": preco_total,
+        "labels": labels,
+        "valores": valores,
+        "years": years,
+        "totals": totals,
+        "paginas_por_ano": paginas_por_ano_ordenado,
+        "total_paginas": total_paginas,
+    })
